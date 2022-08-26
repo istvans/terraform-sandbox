5b20f2069a13282040cf70dc450ce5c982616b33
István Siroki <istvan.siroki@cmrsurgical.com>
2022-08-26 16:12:49 +0100

    Deploying a Cluster of Web Servers

--- main.tf
+++ main.tf
@@ -8,22 +8,14 @@ provider "aws" {
     region = "eu-west-2"
 }
 
-resource "aws_instance" "example" {
-    ami                     = "ami-0f474033296335790"
-    instance_type           = "t2.micro"
-    vpc_security_group_ids  = [aws_security_group.instance.id]
+data "aws_vpc" "get_default" {
+    default = true
+}
 
-    user_data = <<-EOF
-        #!/usr/bin/env bash
-
-        echo "Hello World!" > index.html
-        nohup busybox httpd -f -p ${var.server_port} &
-    EOF
-
-    user_data_replace_on_change = true
-
-    tags = {
-        Name = "terraform-example"
+data "aws_subnets" "get_default" {
+    filter {
+        name   = "vpc-id"
+        values = [data.aws_vpc.get_default.id]
     }
 }
 
@@ -38,7 +30,33 @@ resource "aws_security_group" "instance" {
     }
 }
 
-output "public_ip" {
-    value       = aws_instance.example.public_ip
-    description = "The public IP address of the web server"
+resource "aws_launch_configuration" "example" {
+    image_id                = "ami-0f474033296335790"
+    instance_type           = "t2.micro"
+    security_groups         = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+        #!/bin/bash
+        echo "Hello, World" > index.html
+        nohup busybox httpd -f -p ${var.server_port} &
+    EOF
+
+    # Required when using a launch configuration with an auto scaling group.
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+resource "aws_autoscaling_group" "example" {
+    launch_configuration = aws_launch_configuration.example.name
+    vpc_zone_identifier  = data.aws_subnets.get_default.ids
+
+    min_size = 2
+    max_size = 10
+
+    tag {
+        key                 = "Name"
+        value               = "terraform-asg-example"
+        propagate_at_launch = true
+    }
 }
